d5037cc6c264444adbafb08a1de9c267920c9e82
Gustavo Araujo <gustavob.d.d@gmail.com>
2023-01-20 13:26:29 -0300

    Criacao do projeto Spring e das models usando annotations para relacionar as tabelas

---
+++ academiacxbd/src/main/java/com/academiacxbd/atividadebd/model/ItemModel.java
@@ -0,0 +1,22 @@
+package com.academiacxbd.atividadebd.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_item")
+public class ItemModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantidade;
+    private Double total;
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private ProdutoModel produtoModel;
+    @ManyToOne
+    @JoinColumn(name = "preco_id")
+    private PrecoModel precoModel;
+    @ManyToOne
+    @JoinColumn(name = "carrinho_id")
+    private CarrinhoModel carrinhoModel;
+}
