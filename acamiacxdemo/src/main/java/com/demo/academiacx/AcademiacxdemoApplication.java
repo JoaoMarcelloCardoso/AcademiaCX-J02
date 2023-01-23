6bcdaf57541ff61c5fcb5b60e7a346b7ce5908fe
Vinícius Pereira da Silva Jardim <viniciuspsjardim@gmail.com>
2023-01-22 23:33:00 -0300

    First Commit
    Signed-off-by: Vinícius Pereira da Silva Jardim <viniciuspsjardim@gmail.com>

--- acamiacxdemo/src/main/java/com/demo/academiacx/AcademiacxdemoApplication.java
+++ acamiacxdemo/src/main/java/com/demo/academiacx/AcademiacxdemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AcamiacxdemoApplication {
+public class AcademiacxdemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AcamiacxdemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiacxdemoApplication.class, args);
+	}
 
 }
