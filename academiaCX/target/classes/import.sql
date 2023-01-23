a32dd2a65bb8a67be864c58cb97b14e37e454641
Igor <75454521+igorfilgueira@users.noreply.github.com>
2023-01-23 07:48:20 -0300

    Update import.sql

--- academiaCX/target/classes/import.sql
+++ academiaCX/target/classes/import.sql
@@ -1,22 +1,37 @@
-INSERT INTO tb_cliente(id, cpf, nome) VALUES (1, '12345678901', "João Silva");
-INSERT INTO tb_cliente(id, cpf, nome) VALUES (2, "23456789012", "Maria Costa");
-INSERT INTO tb_cliente(id, cpf, nome) VALUES (3, "34567890123", "José Ferreira");
+INSERT INTO tb_cliente (nome, cpf) VALUES ('João Silva','12345678901');
+INSERT INTO tb_cliente (nome, cpf) VALUES ('Maria Costa','23456789012');
+INSERT INTO tb_cliente (nome, cpf) VALUES ('José Ferreira','34567890123');
 
-INSERT INTO tb_produto(id, sku, nome) VALUES (1, "SKU0001", "Placa de vídeo");
-INSERT INTO tb_produto(id, sku, nome) VALUES (2, "SKU0002", "Memória RAM");
-INSERT INTO tb_produto(id, sku, nome) VALUES (3, '773qwx3vb', "SSD");
+INSERT INTO tb_produto (nome, sku) VALUES ('Placa de Vídeo','1');
+INSERT INTO tb_produto (nome, sku) VALUES ('Memória RAM','2');
+INSERT INTO tb_produto (nome, sku) VALUES ('HD','3');
+INSERT INTO tb_produto (nome, sku) VALUES ('SSD','4');
+INSERT INTO tb_produto (nome, sku) VALUES ('Processador','5');
+INSERT INTO tb_produto (nome, sku) VALUES ('Placa Mãe','6');
 
 INSERT INTO tb_preco(id, valor, produto_id, cliente_id) VALUES (1, 100.00, 123, 1);
 INSERT INTO tb_preco(id, valor, produto_id, cliente_id) VALUES (2, 200.00, 789, 2);
 INSERT INTO tb_preco(id, valor, produto_id, cliente_id) VALUES (3, 50.00, 121, 3);
 
-INSERT INTO tb_carrinho(id, datahora, total, cliente_id) VALUES (1, '2023/01/02', 10, 1);
-INSERT INTO tb_carrinho(id, datahora, total, cliente_id) VALUES (2, '2022/12/24', 20, 2);
-INSERT INTO tb_carrinho(id, datahora, total, cliente_id) VALUES (3, '2022/09/12', 10, 3);
+INSERT INTO tb_carrinho(id, datahora, total, cliente_id) VALUES (1, '2023/01/12', 10, 1);
+INSERT INTO tb_carrinho(id, datahora, total, cliente_id) VALUES (2, '2022/12/14', 20, 2);
+INSERT INTO tb_carrinho(id, datahora, total, cliente_id) VALUES (3, '2022/09/19', 10, 3);
 
-INSERT INTO tb_endereco(id, cep, logradouro, bairro, cidade, uf, cliente_id) VALUES (1,'12345678', 'Rua dos Bobos', '0', 'Bairro dos Bobos', 'Cidade dos Bobos', 'PR', 1);
-INSERT INTO tb_endereco(id, cep, logradouro, bairro, cidade, uf, cliente_id) VALUES (2, '87654321', 'Avenida das Flores', '1', 'Bairro das Flores', 'Cidade das Flores', 'PR', 1);
-INSERT INTO tb_endereco(id, cep, logradouro, bairro, cidade, uf, cliente_id) VALUES (3, '11111111', 'Rua dos Pássaros', '2', 'Bairro dos Pássaros', 'Cidade dos Pássaros', 'PR', 2);
+INSERT INTO tb_endereco (logradouro, numero, bairro, cidade, uf, cliente_id)
+VALUES ('Rua dos Bobos','156','Bairro dos Bobos','Cidade dos Bobos','PR',(SELECT id FROM tb_cliente WHERE cpf='12345678901'));
+INSERT INTO tb_endereco (logradouro, numero, bairro, cidade, uf, cliente_id)
+VALUES ('Avenida das Flores','23','Bairro das Flores','Cidade das Flores','PR',(SELECT id FROM tb_cliente WHERE cpf='23456789012'));
+INSERT INTO tb_endereco (logradouro, numero, bairro, cidade, uf, cliente_id)
+VALUES ('Rua dos Pássaros','555','Bairro dos Pássaros','Cidade dos Pássaros','PR',(SELECT id FROM tb_cliente WHERE cpf='34567890123'));
+INSERT INTO tb_endereco (logradouro, numero, bairro, cidade, uf, cliente_id)
+VALUES ('Avenida dos Bichos','6457','Bairro dos Bichos','Cidade dos Bichos','PR',(SELECT id FROM tb_cliente WHERE cpf='23456789012'));
+INSERT INTO tb_endereco (logradouro, numero, bairro, cidade, uf, cliente_id)
+VALUES ('Rua dos Animais','253','Bairro dos Animais','Cidade dos Animais','PR',(SELECT id FROM tb_cliente WHERE cpf='12345678901'));
+INSERT INTO tb_endereco (logradouro, numero, bairro, cidade, uf, cliente_id)
+VALUES ('Avenida das Árvores','865','Bairro das Árvores','Cidade das Árvores','PR',(SELECT id FROM tb_cliente WHERE cpf='23456789012'));
+INSERT INTO tb_endereco (logradouro, numero, bairro, cidade, uf, cliente_id)
+VALUES ('Rua dos Rios','988','Bairro dos Rios','Cidade dos Rios','PR',(SELECT id FROM tb_cliente WHERE cpf='34567890123'));
+
 
 INSERT INTO tb_item(id, quantidade, total, produto_id, preco_id, carrinho_id) VALUES (1, 10, 50.00, 123, 1, 1);
 INSERT INTO tb_item(id, quantidade, total, produto_id, preco_id, carrinho_id) VALUES (2, 100, 30.00, 456, 2, 2);
