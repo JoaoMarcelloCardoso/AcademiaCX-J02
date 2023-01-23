2f1a6cff29ac2cb64da6cacf79422dcecd5022df
Igor <75454521+igorfilgueira@users.noreply.github.com>
2023-01-23 00:30:07 -0300

    Add files via upload

---
+++ academiaCX/src/main/java/com/example/academiacx/model/AcademiacxexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.academiacx.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiacxexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademiacxexampleApplication.class, args);
+    }
+
+}
